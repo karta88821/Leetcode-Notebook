0dc3bbc0a61fb7f41731d2998462ec69c937f895
Windows10-Home <karta88821@gmail.com>
2022-12-16 01:09:20 +0800

    add the desc of the problem #763

--- 763.cpp
+++ 763.cpp
@@ -1,3 +1,19 @@
+/*
+#763. Partition Labels
+
+You are given a string s. We want to partition the string into as many parts as possible so 
+that each letter appears in at most one part.
+
+Note that the partition is done so that after concatenating all the parts in order, the resultant 
+string should be s.
+
+Return a list of integers representing the size of these parts.
+
+Example:
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+*/
+
 class Solution {
 public:
     // Time:  O(n), where n is the size of the input string
